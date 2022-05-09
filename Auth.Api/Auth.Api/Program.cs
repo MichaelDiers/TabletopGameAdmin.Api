ef8ac2371d75b5192ef6c8e49ca0e3159b6c7d55
MichaelDiers <md@sreid.de>
2022-05-09 14:53:15 +0200

    service startup fixes

--- Auth.Api/Auth.Api/Program.cs
+++ Auth.Api/Auth.Api/Program.cs
@@ -1,5 +1,6 @@
 namespace Auth.Api
 {
+    using System;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Hosting;
 
@@ -8,7 +9,14 @@ namespace Auth.Api
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(
+                    webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>();
+                        var portHttp = Environment.GetEnvironmentVariable("PORT") ?? "5003";
+                        var portHttps = int.TryParse(portHttp, out var port) ? port + 1 : 5004;
+                        webBuilder.UseUrls($"https://localhost:{portHttps}", $"http://localhost:{portHttp}");
+                    });
         }
 
         public static void Main(string[] args)
