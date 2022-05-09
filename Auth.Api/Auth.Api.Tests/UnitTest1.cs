bc87ee1a6c03ff2110b49df30c9787ee03e3cdfd
MichaelDiers <md@sreid.de>
2022-05-09 11:06:22 +0200

    build and test

---
+++ Auth.Api/Auth.Api.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+namespace Auth.Api.Tests
+{
+    using Xunit;
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
